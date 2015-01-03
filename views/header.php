9a9e6fe929ea2b62f7356f86c5950e74b9fa82a0
James <jgebczak@gmail.com>
2015-01-03 00:03:50 -0800

    Add Font Awesome as vendor plugin

--- views/header.php
+++ views/header.php
@@ -15,7 +15,7 @@
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/default.css">
     <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="/vendor/font-awesome-4.2.0/css/font-awesome.min.css">
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
