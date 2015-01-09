da6a4f44ca13d73aad1b1b4f8f5fb70ac44242cb
James <jgebczak@gmail.com>
2015-01-08 18:34:46 -0800

    Table/Create: started

--- views/sidebar.php
+++ views/sidebar.php
@@ -38,6 +38,10 @@
 
         <!-- LIST OF TABLES -->
 
+            <div style='text-align:center'>
+                <a style='font-size:0.9em; color:purple' href="<?=Box::url(array('create'=>''))?>">+ Create new table</a>
+            </div>
+
             <div class='container bl_gray_13 sidebar_tables'>
 
                 <?php if ($tables = Box::getTables()): ?>
